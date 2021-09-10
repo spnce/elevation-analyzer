971b99c80a5b20f10c40f58e08d0f49be316babd
Spencer McNamara <snmcnamara@gmail.com>
2021-09-10 07:16:32 -0700

    Add default values for ll and elev

--- src/station.py
+++ src/station.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
 
 @dataclass_json
@@ -7,5 +7,5 @@ class Station:
     uid: int
     state: str
     name: str
-    ll: str
-    elev: float
+    ll: str = field(default="Unknown")
+    elev: float = field(default=None)
