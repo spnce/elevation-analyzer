bcbe5ac847625b57affea511c36b1277ad0f53b5
Spencer McNamara <snmcnamara@gmail.com>
2021-09-10 07:09:50 -0700

    Add function to build a report

--- src/station_report.py
+++ src/station_report.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 from typing import List
 from station import Station
+from dataclass_csv import DataclassReader
+from statistics import mean, median
 
 @dataclass_json
 @dataclass
@@ -16,3 +18,29 @@ class StationReport:
     highest_stations: List[Station]
     lowest_stations: List[Station]
     median_stations: List[Station]
+
+
+def _open_stations(stations_file: str, state: str) -> List[Station]:
+    with open(stations_file) as stations:
+        return [station for station in DataclassReader(stations, Station) if station.state == state]
+
+
+def build_report(stations_file: str, state: str) -> StationReport:
+    stations = _open_stations(stations_file, state)
+
+    elevations = [station.elev for station in stations]
+    median_elevation = median(elevations)
+    max_elevation = max(elevations)
+    min_elevation = min(elevations)
+
+    return StationReport(
+        state=state,
+        station_count=len(stations),
+        maximum_elevation=max_elevation,
+        minimum_elevation=min_elevation,
+        average_elevation=mean(elevations),
+        median_elevation=median_elevation,
+        highest_stations=[station for station in stations if station.elev == max_elevation],
+        lowest_stations=[station for station in stations if station.elev == min_elevation],
+        median_stations = [station for station in stations if station.elev == median_elevation],
+    )
