77bddf9732380837a801ef445e9b59944c5bc529
Spencer McNamara <snmcnamara@gmail.com>
2021-09-10 08:03:51 -0700

    Add wrapperscript

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 import argparse
 from station_report import build_report
 
-parser = argparse.ArgumentParser(description="Analyze climate station elevation by state.")
-parser.add_argument('stations', type=str, help="CSV containing station elevation data.")
-parser.add_argument('--state', required=True, type=str, help="State you'd like to generate a report for.")
+def _parse_args():
+    parser = argparse.ArgumentParser(prog="analyze-elevation", description="Analyze climate station elevation by state.")
+    parser.add_argument('stations', type=str, help="CSV containing station elevation data.", metavar="STATIONS_FILE")
+    parser.add_argument('--state', required=True, type=str, help="State you'd like to generate a report for.")
 
-args = parser.parse_args()
-report = build_report(args.stations, args.state)
-report.dump()
+    return parser.parse_args()
+
+def _write_report(station_file: str, state: str):
+    report = build_report(station_file, state)
+    report.dump()
+
+def run():
+    args = _parse_args()
+    _write_report(args.stations, args.state)
+
+run()
