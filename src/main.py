ce0b847041188a55dc0c7ebc269ae538ca2bb85e
Spencer McNamara <snmcnamara@gmail.com>
2021-09-10 07:16:11 -0700

    Make state a required arg, run the report builder

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import argparse
+from station_report import build_report
 
 parser = argparse.ArgumentParser(description="Analyze climate station elevation by state.")
 parser.add_argument('stations', type=str, help="CSV containing station elevation data.")
-parser.add_argument('--state', type=str, help="State you'd like to generate a report for.")
+parser.add_argument('--state', required=True, type=str, help="State you'd like to generate a report for.")
 
 args = parser.parse_args()
+report = build_report(args.stations, args.state)
+print(report.to_json())
